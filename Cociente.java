b781d3b919f5cf2d91042f859aafadcec64a198c
danielalberich <danielalberichmayoral@gmail.com>
2021-02-21 11:32:19 +0100

    Añado método que divide dos números enteros.

---
+++ Cociente.java
@@ -0,0 +1,30 @@
+
+/**
+ * 
+ * @author Daniel
+ * @version 1.0
+ * @since 28/01/2021
+ * 
+ */
+
+public class Cociente {
+
+	/**
+	 * Metodo en el que divide dos numeros enteros.
+	 * 
+	 * @param dividendo El numero a dividir.
+	 * @param divisor El numero entre el que se divide.
+	 * @return Devuelve la division.
+	 * 
+	 * Si el dividendo es 0 el resultado sera 0.
+	 * Si el divisor es 0 el resultado sera infinito.
+	 * Si ambos son 0 el resultado dara Error.
+	 * 
+	 * En caso de no dar un número exacto el resultado omite la parte decimal.
+	 */
+	public static int dosEnteros (int dividendo, int divisor) {
+		return dividendo/divisor;
+	}
+	
+	
+}
