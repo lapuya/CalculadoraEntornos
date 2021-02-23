74abf4373d03e3cf99213934f8603eff35e4f1a7
Íván <ivanpm89@gmail.com>
2021-02-23 20:26:32 +0100

    Producto v1

---
+++ Producto.java
@@ -0,0 +1,27 @@
+public class Producto {
+ 
+	
+		public double productoDosReales(double n1, double n2) {
+			return n1*n2;
+		}
+		
+ 
+		public int productoDosEnteros(int n1, int n2) {
+			return n1*n2;
+		}
+ 
+		
+ 
+		public int productoTresReales(int n1, int n2, int n3) {
+			return n1*n2*n3;
+		}
+ 
+		
+		public double potencia(double n1, double n2) {
+			return Math.pow(n1, n2);
+		}
+ 
+ 
+	}
+ 
+ 
