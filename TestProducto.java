b1dc614fef41bf4b7e64fedea620b5c0345c70a8
Fran Gonzalez <frangonzalezh1414@gmail.com>
2021-03-07 19:16:10 +0100

    actualizacion de TestProducto

--- TestProducto.java
+++ TestProducto.java
@@ -2,37 +2,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class CocienteTest {
-	Cociente cut = new Cociente();
-	@Test
-	void testDosEnteros() {
-		//Arrange
-		
-		//Act
-		
-		//Assert
+class TestProducto {
+	private Producto cut = new Producto();
+	public void setUp() {
+				cut = new Producto();
 	}
 
 	@Test
-	void testDosReales() {
-		//Arrange
-		double num1= 6.6;
-		double num2= 6.6;
-		double esperado = 1.0;
-		//Act
-		double resultado =cut.dosReales(num1, num2);
-		//Assert
+	void testNumerosDosReales() {
+		// Arrange - Preparar
+		double num1= 5.5;
+		double num2=5.5;
+		double esperado =30.25;
+		// Act - ejecutar
+		double obtenido=cut.productoDosReales(num1, num2);
+		// Assert - comprobar
+		assertEquals(esperado, obtenido,"el producto de 5.5 por 5.5 debe ser 30.25");
+	
+	}
+
+	@Test
+	void testProductoDosEnteros() {
+		// Arrange - Preparar
+		int num1= 5;
+		int num2=5;
+		int esperado =25;
+		// Act - ejecutar
+		int obtenido = cut.productoDosEnteros(num1, num2);
+		// Assert - comprobar
+		assertEquals(esperado, obtenido,"el producto de 5 por 5 debe ser 25");
+	
+	}
+
+	@Test
+	void testProductoTresReales() {
+		//Arrange - Preparar
+		double num1=5.5;
+		double num2=2.5;
+		double num3=2.5;
+		double esperado= 34.375;
+		
+		// Act - Ejecutar
+		double resultado = cut.productoTresReales(num1, num2, num3);
+		
+		// Assert - Comprobar
+		assertEquals(esperado, resultado,"resultado esperado de los tres reales es 34.375");
+	}
+
+	@Test
+	void testPotencia() {
+	// Arrange - Preparar
+		double num1 = 5.5;
+		double num2 = 4.5;
+		double esperado = 2146.0117856117135;
+	// Act- Ejecutar
+		double resultado = cut.potencia(num1, num2);
+	// Assert- comprobar
 		assertEquals(esperado, resultado);
-	}
-
-	@Test
-	void testInverso() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testRaiz() {
-		fail("Not yet implemented");
+		
 	}
 
 }
